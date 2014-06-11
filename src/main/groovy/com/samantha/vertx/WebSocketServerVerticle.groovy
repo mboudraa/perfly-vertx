0f2f936ae4a1a6ce6d8eb75848f8141bd96d4a17
Mounir Boudraa <mounir.boudraa@gmail.com>
2014-06-11 17:19:13 +0200

    fix server bot receive websocket connection

--- src/main/groovy/com/samantha/vertx/WebSocketServerVerticle.groovy
+++ src/main/groovy/com/samantha/vertx/WebSocketServerVerticle.groovy
@@ -8,27 +8,26 @@ class WebSocketServerVerticle extends Verticle {
 
     def start() {
         println "Starting Server..."
-        server = createWebSocketServer(container.config.host, container.config.port) { asyncResult ->
-            "Server started. Listening on: ${host}:${port}"
-        }
+        server = createWebSocketServer(container.config.host, container.config.port)
     }
 
-    def createWebSocketServer(host, port, closure) {
+    def createWebSocketServer(host, port) {
         def httpServer = vertx.createHttpServer()
 
         httpServer.websocketHandler { ws ->
-            println "a websocked has connected"
             ws.dataHandler { buffer ->
                 def message = buffer.getString(0, buffer.length)
                 println message
             }
-        }.listen(port, host, closure)
+        }.listen(port, host)
     }
 
     def stop() {
-        println "Closing Server..."
-        server.close { asyncResult ->
-            println "Server closed"
+        if (server) {
+            println "Closing Server..."
+            server.close { asyncResult ->
+                println "Server closed"
+            }
         }
     }
 
