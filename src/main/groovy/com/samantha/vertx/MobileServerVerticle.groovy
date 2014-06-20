0b036128df2e8fbc667ebdf2bf3d0a045a4bda31
Mounir Boudraa <mounir.boudraa@gmail.com>
2014-06-21 01:21:30 +0200

    parsing all communications received from tcp connection

--- src/main/groovy/com/samantha/vertx/MobileServerVerticle.groovy
+++ src/main/groovy/com/samantha/vertx/MobileServerVerticle.groovy
@@ -1,7 +1,6 @@
 package com.samantha.vertx
 
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.vertx.groovy.core.buffer.Buffer
 import org.vertx.groovy.core.eventbus.Message
 import org.vertx.groovy.core.net.NetSocket
@@ -9,15 +8,18 @@ import org.vertx.groovy.platform.Verticle
 
 class MobileServerVerticle extends Verticle {
 
+    private static final String EOC = "\\|\\|EOC\\|\\|"
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     def server
     NetSocket socket
-    JsonSlurper jsonSlurper = new JsonSlurper()
+
 
     @Override
     def start() {
 
         println "Starting Mobile Server..."
-        server = createWebSocketServer(container.config) { asyncResult, host, port ->
+        server = createSocketServer(container.config) { asyncResult, host, port ->
             if (asyncResult.succeeded) {
                 println "Mobile Server started. Listening on ${host}:${port}"
             } else {
@@ -35,6 +37,7 @@ class MobileServerVerticle extends Verticle {
     @Override
     def stop() {
         println "Closing Mobile Server..."
+        socket?.close()
         server?.close { asyncResult ->
             if (asyncResult.succeeded) {
                 println "Mobile Server closed"
@@ -45,25 +48,29 @@ class MobileServerVerticle extends Verticle {
     }
 
 
-    def createWebSocketServer(config, closure) {
+    def createSocketServer(config, closure) {
 
         vertx.createNetServer().connectHandler { NetSocket socket ->
+
             this.socket = socket
-
-
             Buffer body = new Buffer(0)
-            socket.dataHandler { Buffer buffer ->
-                body << buffer
-//                socket.pause()
-                try {
-                    String message = body.getString(0, body.length)
-                    jsonSlurper.parseText(message)
-                    println message
-                    body = new Buffer(0)
-                } catch (Exception e) {
 
-                } finally {
-//                    socket.resume()
+            socket.dataHandler { Buffer buffer ->
+
+                body << buffer
+                String[] messages = body.getString(0, body.length).split(EOC)
+
+                messages.eachWithIndex { message, i ->
+                    try {
+                        def object = OBJECT_MAPPER.readValue(message, Map.class)
+                        vertx.eventBus.send(object.address, OBJECT_MAPPER.writeValueAsString(object.body.object))
+                        body = new Buffer(0)
+                    } catch (Exception e) {
+
+                        if (i > 0 && i == messages.length - 1) {
+                            body << message.bytes
+                        }
+                    }
                 }
             }
 
